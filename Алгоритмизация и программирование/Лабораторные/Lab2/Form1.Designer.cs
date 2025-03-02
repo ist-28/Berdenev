34ee16139d3d9febc14f3a92a5e96adbbe67fd19
Kapat1ch <warfece207@gmail.com>
2025-03-02 21:32:01 +0500

    Lab2

---
+++ Алгоритмизация и программирование/Лабораторные/Lab2/Form1.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace Lab2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textX = new System.Windows.Forms.TextBox();
+            this.textY = new System.Windows.Forms.TextBox();
+            this.textZ = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textResult = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(30, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(227, 39);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Решение уровнения вида:\r\nt=(2cos⁡(x-π/6))/(0.5+sin^2⁡y)(1+(z^2)/(3-z^2/5))\r\n\r\n";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(32, 54);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(26, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "X = ";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(32, 85);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(26, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Y = ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(32, 112);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(26, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Z = ";
+            // 
+            // textX
+            // 
+            this.textX.Location = new System.Drawing.Point(64, 51);
+            this.textX.Name = "textX";
+            this.textX.Size = new System.Drawing.Size(139, 20);
+            this.textX.TabIndex = 4;
+            // 
+            // textY
+            // 
+            this.textY.Location = new System.Drawing.Point(64, 82);
+            this.textY.Name = "textY";
+            this.textY.Size = new System.Drawing.Size(139, 20);
+            this.textY.TabIndex = 5;
+            // 
+            // textZ
+            // 
+            this.textZ.Location = new System.Drawing.Point(64, 109);
+            this.textZ.Name = "textZ";
+            this.textZ.Size = new System.Drawing.Size(139, 20);
+            this.textZ.TabIndex = 6;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(30, 149);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(189, 13);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Результат выполнения программы:";
+            // 
+            // textResult
+            // 
+            this.textResult.Location = new System.Drawing.Point(33, 175);
+            this.textResult.Multiline = true;
+            this.textResult.Name = "textResult";
+            this.textResult.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textResult.Size = new System.Drawing.Size(224, 151);
+            this.textResult.TabIndex = 8;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(288, 326);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Выполнить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(384, 361);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textResult);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textZ);
+            this.Controls.Add(this.textY);
+            this.Controls.Add(this.textX);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Linear equation";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textX;
+        private System.Windows.Forms.TextBox textY;
+        private System.Windows.Forms.TextBox textZ;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textResult;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Алгоритмизация и программирование/Лабораторные/Lab2/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            textX.Text = "14,26";
+            textY.Text = "-1,22";
+            textZ.Text = "0,035";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double x = double.Parse(textX.Text);
+            double y = double.Parse(textY.Text);
+            double z = double.Parse(textZ.Text);
+
+            textResult.Text += Environment.NewLine + "X = " + x.ToString();
+            textResult.Text += Environment.NewLine + "Y = " + y.ToString();
+            textResult.Text += Environment.NewLine + "Z = " + z.ToString();
+
+            double a = (2 * Math.Cos(x - 3.14 / 6)) / (0.5 + (Math.Sin(y) * Math.Sin(y)));
+            double b = 1 + (Math.Pow(z,2) / (3 - (Math.Pow(z,2)/5)));
+            double c = a * b; 
+
+            textResult.Text += Environment.NewLine + "Результат t = " + c.ToString();
+        }
+    }
+}
