e05bf598688dd34651b57b5c453d6f267e1ff828
Kapat1ch <warfece207@gmail.com>
2025-03-25 10:59:16 +0500

    hskjhf

---
+++ Алгоритмизация и программирование/Лабораторные/Lab6/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab6
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int i = listBox1.SelectedIndex;
+            int zero = 0;
+            int one = 0;
+            string str = (string)listBox1.Items[i];
+            for (int j = 0; j < str.Length; j++) {
+                if (str[j] == '0') { 
+                    zero++;
+                } else
+                {
+                    one++;
+                }
+            }
+            label1.Text = "Кол-во 1 = " + Convert.ToString(one) + "; Кол-во 0 = " + Convert.ToString(zero);
+        }
+    }
+}
